c8d6e744bd091ded73a01092beffb2f6b040a042
alek <wait82985@gmail.com>
2023-06-26 16:41:54 +0600

    porblem solved. Binary search

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,18 @@
 public class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        //TODO
-        return 0;
+
+        int left = 0;
+        int right = arr.length - 1;
+        int mid = left + (right - left) / 2;
+
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+                left = mid;
+            } else right = mid;
+        }
+        return mid;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -28,4 +28,23 @@ public class SolutionTest {
 
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void peakIndexInMountainArrayTest4() {
+        int[] arr = {0, 1, 2, 3, 4, 5, 10, 5, 2};
+        int expected = 6;
+        int actual = new Solution().peakIndexInMountainArray(arr);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void peakIndexInMountainArrayTest5() {
+        int[] arr = {3,5,3,2,0};
+        int expected = 1;
+        int actual = new Solution().peakIndexInMountainArray(arr);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
